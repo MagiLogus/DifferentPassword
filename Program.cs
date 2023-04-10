37cfd1e5fccb8e8d2f2cec6a15c8d8f4a60f74aa
Paulo Henrique Gonçalves de Oliveira <paulo.oliveira.phgo@gmail.com>
2023-04-10 10:28:51 -0300

    exercicico executado

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿//Faça um programa que leia um nome de usuario e a sua senha e não aceite a senha igual ao usuario, mostrando uma mensagem de erro e voltando a pedir as informações.
+
+//Entrada
+Console.WriteLine($"Insira seu nome de usuário: ");
+string usuario = Console.ReadLine();
+Console.WriteLine($"Insira sua senha: ");
+string senha = Console.ReadLine();
+
+
+//Processamento 
+while (senha == usuario)
+{
+Console.WriteLine("Erro: a senha não pode ser igual ao nome de usuário. Tente novamente.");
+Console.WriteLine($"Insira sua senha: ");
+senha = Console.ReadLine();
+}
+Console.WriteLine($"Seu cadastro foi realizado com sucesso!");
+
+//Saida
+
